6c303ebb162ff0cab2a9abe8df933430e81f0573
Demetrio <demetrioalmeida@gmail.com>
2020-10-10 11:50:55 -0400

    contactComponent updated

--- client/src/components/PortfolioCard/index.js
+++ client/src/components/PortfolioCard/index.js
@@ -4,27 +4,22 @@ import cards from "../../utils/cards";
 const Card = () => {
   console.log(cards);
 
-  const cardsEl = cards.map((card) => (
-    <div className="col mb-4" key={card.id}>
+  const cardsEl = cards.map(({ id, src, href, appName, text }) => (
+    <div className="col mb-4" key={id}>
       <div className="card">
-        <img
-          className="card-img-top"
-          width="150px"
-          height="250px"
-          src={card.src}
-          alt={card.appName}
-        />
+        <img className="card-img-top" src={src} alt={appName} />
         <div className="card-body">
           <h5 className="card-title">
-            <a href={card.href} target="_blank" rel="noopener noreferrer">
-              {card.appName}
+            <a href={href} target="_blank" rel="noopener noreferrer">
+              {appName}
             </a>
           </h5>
-          <p className="card-text">{card.text}</p>
+          <p className="card-text">{text}</p>
         </div>
       </div>
     </div>
   ));
+
   return (
     <main className="col-12 col-md-12 col-xl-12 py-md-3 pl-md-5 bd-content">
       <div className="row row-cols-1 row-cols-md-3">{cardsEl}</div>
--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -4,8 +4,8 @@ import Footer from "../../components/Footer";
 import emailjs from "emailjs-com";
 import "./style.css";
 
-const Contact = (props) => {
-  function sendEmail(e) {
+class Contact extends React.Component {
+  sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
@@ -26,57 +26,61 @@ const Contact = (props) => {
       );
 
     e.target.reset();
-  }
+  };
 
-  return (
-    <React.Fragment>
-      <Header {...props} />
-      <main className="col-12 col-md-9 col-xl-12 py-md-3 pl-md-5 bd-content">
-        <div className="row row-cols-1 row-cols-md-3">
-          <div className="col-md-6 offset-md-3">
-            <form className="contact-form" onSubmit={sendEmail}>
-              <div className="form-group">
-                <label htmlFor="exampleFormControlInput1">Name</label>
+  render() {
+    return (
+      <React.Fragment>
+        <Header {...this.props} />
+        <main className='col-12 col-md-9 col-xl-12 py-md-3 pl-md-5 bd-content'>
+          <div className='row row-cols-1 row-cols-md-3'>
+            <div className='col-md-6 offset-md-3'>
+              <form className='contact-form' onSubmit={this.sendEmail}>
+                <div className='form-group'>
+                  <label htmlFor='exampleFormControlInput1'>Name</label>
+                  <input
+                    type='text'
+                    className='form-control'
+                    name='from_name'
+                    placeholder='Full name'
+                  />
+                </div>
+                <div className='form-group'>
+                  <label htmlFor='exampleFormControlInput1'>
+                    Email address
+                  </label>
+                  <input
+                    type='email'
+                    className='form-control'
+                    name='from_email'
+                    placeholder='name@example.com'
+                  />
+                </div>
+                <div className='form-group'>
+                  <label htmlFor='exampleFormControlInput1'>Subject</label>
+                  <input type='text' className='form-control' name='subject' />
+                </div>
+                <div className='form-group'>
+                  <label htmlFor='exampleFormControlTextarea1'>Message</label>
+                  <textarea
+                    className='form-control'
+                    name='message'
+                    rows='3'
+                  ></textarea>
+                </div>
                 <input
-                  type="text"
-                  className="form-control"
-                  name="from_name"
-                  placeholder="Full name"
+                  type='submit'
+                  className='btn btn-primary'
+                  value='Send Message'
                 />
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleFormControlInput1">Email address</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  name="from_email"
-                  placeholder="name@example.com"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleFormControlInput1">Subject</label>
-                <input type="text" className="form-control" name="subject" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleFormControlTextarea1">Message</label>
-                <textarea
-                  className="form-control"
-                  name="message"
-                  rows="3"
-                ></textarea>
-              </div>
-              <input
-                type="submit"
-                className="btn btn-primary"
-                value="Send Message"
-              />
-            </form>
+              </form>
+            </div>
           </div>
-        </div>
-      </main>
-      <Footer />
-    </React.Fragment>
-  );
-};
+        </main>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
 
 export default Contact;
